4bd88427f68bc57e206126a0b1220dd2995b6694
RIZKA SUGIHARTO <72377723+RizkaSugiharto@users.noreply.github.com>
2020-10-08 13:40:57 +0700

    Add files via upload

---
+++ perulangan5.js
@@ -0,0 +1,49 @@
+<html>
+<body>
+<script>
+  var baris = 5;
+var polaBintang = "*";
+var polaSamaDgn = "=";
+var hasil = "";
+
+for (let indexx = 1; indexx < 10; indexx++) {
+	if ((indexx %2)==0) {
+	hasil = hasil.concat(polaSamaDgn);
+	} else { 
+	hasil = hasil.concat(polaBintang);
+	}
+}
+console.log(""+hasil);
+
+hasil="";
+for (let indexx = 1; indexx < 8; indexx++) {
+	if ((indexx %2)==0) {
+	hasil = hasil.concat(polaSamaDgn);
+	} else { 
+	hasil = hasil.concat(polaBintang);
+	}
+}
+console.log(" "+hasil);
+
+hasil="";
+for (let indexx = 1; indexx < 6; indexx++) {
+	if ((indexx %2)==0) {
+	hasil = hasil.concat(polaSamaDgn);
+	} else { 
+	hasil = hasil.concat(polaBintang);
+	}
+}
+console.log(" "+hasil);
+hasil="";
+for (let indexx = 1; indexx < 4; indexx++) {
+	if ((indexx %2)==0) {
+	hasil = hasil.concat(polaSamaDgn);
+	} else { 
+	hasil = hasil.concat(polaBintang);
+	}
+}
+console.log(" " +hasil);
+console.log("  " +polaBintang);
+</script>
+</body>
+</html>
